1cb0c853f2613b50f01f3b2202e1e482d4fbdf6a
Sebastien Douheret <sebastien.douheret@iot.bzh>
2021-01-31 23:24:01 +0100

    Add serve/daemon mode (--serve option)
    Change-Id: I4b8db77eeda427b4fbd8aa9adb1c625bef4b3d4d
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- src/index.ts
+++ src/index.ts
@@ -27,97 +27,117 @@ import { TarballRegistryBackend } from './fs_registry';
 import { DirRegistryBackend } from './dir_registry'
 
 const concurrent_processes = 100;
-const install_options:string[] = [];
+const install_options: string[] = [];
 
-function registerTarballsFromCommandline(registry:Registry): Promise<number> {
-	const processes:Promise<number>[] = [];
-	for (let i=0; i<concurrent_processes; i++)
-		processes.push(Promise.resolve(0));
-	for (let i=2; i<argv.length; ++i) {
-		processes[i%concurrent_processes] = processes[i%concurrent_processes].then((processed) => registry.register(argv[i]).then(n => {
-			if (n == 0)
-				install_options.push(argv[i]);
+function registerTarballsFromCommandline(registry: Registry): Promise<number> {
+    const processes: Promise<number>[] = [];
+    for (let i = 0; i < concurrent_processes; i++)
+        processes.push(Promise.resolve(0));
+    for (let i = 2; i < argv.length; ++i) {
+        processes[i % concurrent_processes] = processes[i % concurrent_processes].then((processed) => registry.register(argv[i]).then(n => {
+            if (n == 0)
+                install_options.push(argv[i]);
 
-			return processed + n;
-		}));
-	}
+            return processed + n;
+        }));
+    }
 
-	return Promise.all(processes).then((vals) => {
-		let total = 0;
-		for (let i=0; i<vals.length; ++i)
-			total += vals[i];
+    return Promise.all(processes).then((vals) => {
+        let total = 0;
+        for (let i = 0; i < vals.length; ++i)
+            total += vals[i];
 
-		console.log(`Serving ${total} packages`);
-		return total;
-	});
+        console.log(`Serving ${total} packages`);
+        return total;
+    });
 }
 
-function setupServerAndGetPort(service:Service, registry:Registry): Promise<number> {
-	return new Promise(accepted => {
-		const server = service.run(registry).on("listening", () => {
-			const addr = server.address();
-			console.log(addr);
-			if (typeof addr === 'object')
-				accepted(addr.port);
-		});
-	});
+function setupServerAndGetPort(service: Service, registry: Registry): Promise<number> {
+    return new Promise(accepted => {
+        const server = service.run(registry).on("listening", () => {
+            const addr = server.address();
+            console.log(addr);
+            if (typeof addr === 'object')
+                accepted(addr.port);
+        });
+    });
 }
 
-function configureNpmToSpecificLocalhostPort(service:Service, port:number|Promise<number>): Promise<void> {
-	return new Promise((accept, reject) => {
-		spawn("/usr/bin/npm", ['config', 'set', 'registry', service.url.toString()], {stdio: 'inherit'})
-		.on("exit", (code) => {
-			code === 0 ? accept() : reject();
-		});
-	});
+function configureNpmToSpecificLocalhostPort(service: Service, port: number | Promise<number>): Promise<void> {
+    return new Promise((accept, reject) => {
+        spawn("/usr/bin/npm", ['config', 'set', 'registry', service.url.toString()], { stdio: 'inherit' })
+            .on("exit", (code) => {
+                code === 0 ? accept() : reject();
+            });
+    });
 }
 
 function runNpmInstall(): Promise<void> {
-	if (install_options.length === 0) {
-		console.log("npm install skipped");
-		return Promise.reject("npm install skipped");
-	}
+    if (install_options.length === 0) {
+        console.log("npm install skipped");
+        return Promise.reject("npm install skipped");
+    }
 
-	return new Promise((accept, reject) => {
-		spawn("/usr/bin/npm", install_options, {stdio: 'inherit'})
-		.on("exit", (code) => {
-			code === 0 ? accept() : reject("NPM returned code: " + code);
-		});
-	});
+    return new Promise((accept, reject) => {
+        spawn("/usr/bin/npm", install_options, { stdio: 'inherit' })
+            .on("exit", (code) => {
+                code === 0 ? accept() : reject("NPM returned code: " + code);
+            });
+    });
 }
 
 function printHelpInformation() {
-	console.log("   usage: index [ npm files | npm tarball directories ] ... [npm run options] ");
-	console.log("--help    prints this help message");
+    console.log("   usage: index [--help] [--serve] [ npm files | npm tarball directories ] ... [npm run options]");
+    console.log("--serve   run registry as a 'daemon'");
+    console.log("--help    prints this help message");
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 function mainEntryFunction(): void {
-	if (argv.includes("--help")) {
-		printHelpInformation();
-	}
+    let serve = false;
+    if (argv.includes("--help")) {
+        printHelpInformation();
+    }
+    if (argv.includes("--serve")) {
+        serve = true;
+    }
 
-	const registry = new Registry();
-	registry.addBackend(new TarballRegistryBackend);
-	registry.addBackend(new DirRegistryBackend);
-	const service = new Service({url: new URL("http://localhost")});
-	registry.serviceProvider = service;
+    const registry = new Registry();
+    registry.addBackend(new TarballRegistryBackend);
+    registry.addBackend(new DirRegistryBackend);
+    const service = new Service({ url: new URL("http://localhost") });
+    registry.serviceProvider = service;
 
-	registerTarballsFromCommandline(registry)
-	.then(() => setupServerAndGetPort(service, registry))
-	.then(port => configureNpmToSpecificLocalhostPort(service, port))
-	.then(() => runNpmInstall())
-	.then(() => {
-		console.log("npm done. Shutting down proxy");
-		return service.stop()
-	})
-	.catch(msg => {
-		console.log("An error occurred: " + msg);
-		process.exit(1);
-	});
+    console.log("Initializing registry...");
+    registerTarballsFromCommandline(registry)
+        .then(() => setupServerAndGetPort(service, registry))
+        .then(port => configureNpmToSpecificLocalhostPort(service, port))
+        .then(() => {
+            if (serve) {
+                console.log("Npm local server running...");
+                return sleep(24 * 60 * 60 * 100);
+            } else {
+                console.log("Install packaging: npm ", install_options);
+                return runNpmInstall();
+            }
+        })
+        .then(() => {
+            console.log("npm done. Shutting down proxy");
+            return service.stop()
+        })
+        .catch(msg => {
+            console.log("An error occurred: " + msg);
+            process.exit(1);
+        });
 }
 
 if (require.main === module)
-	mainEntryFunction();
+    mainEntryFunction();
 
 export { mainEntryFunction }
 
